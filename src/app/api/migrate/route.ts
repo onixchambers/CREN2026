fafb3ed61c234566800c8a7caeb35fe703a6ce15
Onix Chambers <onixc@example.com>
2026-07-24 17:03:10 -0500

    Change migrate script to debug users

--- src/app/api/migrate/route.ts
+++ src/app/api/migrate/route.ts
@@ -3,8 +3,8 @@ import { prisma } from '@/lib/prisma';
 
 export async function GET() {
   try {
-    await prisma.$executeRawUnsafe(`ALTER TABLE "User" ADD COLUMN "especialidad" TEXT;`);
-    return NextResponse.json({ success: true, message: 'Migration successful!' });
+    const users = await prisma.user.findMany();
+    return NextResponse.json({ success: true, users });
   } catch (error: any) {
     return NextResponse.json({ success: false, error: error.message });
   }
